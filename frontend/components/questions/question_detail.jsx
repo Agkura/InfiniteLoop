6e4deeed9cfddc612f6adf6a99b929447b3562a2
agkura <agkura.code@gmail.com>
2017-05-18 18:00:10 -0700

    Add question index box and basic styling

--- frontend/components/questions/question_detail.jsx
+++ frontend/components/questions/question_detail.jsx
@@ -6,9 +6,22 @@ class QuestionDetail extends React.Component{
   }
 
   render(){
+    let { title, username, created_at } = this.props.question;
     return(
-      <div>
-        {this.props.question.title}
+      <div className="question-bar">
+        <div className="upvote-block">
+          
+        </div>
+        <div className="question-grid">
+          <ul className="question-upper">
+            <li className="question-title"><h3>{title}</h3></li>
+          </ul>
+          <ul className="question-lower">
+            <li className="question-fill-lower"></li>
+            <li className="question-created-at">{created_at}</li>
+            <li className="question-username">{username}</li>
+          </ul>
+        </div>
       </div>
     )
   }
